673158c6b185b6705300eb98539a251dc4563baf
Mukti <mukti.devmail@gmail.com>
2024-01-24 12:25:48 +0700

    Revert "Package development phase 3"
    This reverts commit bf78ae8e9258db6fd22fef6e1ceec17df2c5090f.

--- appgen/stubs/routes/generator.php
+++ appgen/stubs/routes/generator.php
@@ -14,10 +14,7 @@ use App\Http\Controllers\MLabGeneratorController;
 |
 */
 
-// Main Routes
 Route::get('mlab-generator', [MLabGeneratorController::class, 'index'])->name('generator.index');
 Route::get('crud-manager', [MLabGeneratorController::class, 'crudManager'])->name('generator.crud');
 Route::get('user-manager', [MLabGeneratorController::class, 'userManager'])->name('generator.user');
-
-// Helper Routes
-Route::post('setup-app', [MLabGeneratorController::class, 'crudmanGenerateModel'])->name('generator.createModel');
+Route::get('user-manager', [MLabGeneratorController::class, 'crudmanGenerateModel'])->name('generator.createModel');
